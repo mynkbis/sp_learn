b6dca69beb80b506b4bea92a198515da6fc7e8fa
mynkbis <bisht.surya@ymail.com>
2022-05-25 13:22:43 +0530

    button fixed

--- learn/src/App.js
+++ learn/src/App.js
@@ -3,16 +3,13 @@ import './App.css';
 import Button from './Button/button';
 
 
-const pop=()=>{
-  alert("you clicked me ")
-}
-
 function App() {
   return (
     <div className="App">
 
-<div onClick={pop}>{Button("Submit","150px","50px","Black","green")}</div>
-<div>{Button("Press-me","150px","50px","Black","green")}</div>
+<div onClick={()=>{ alert("You clicked submit")}}>{Button("Submit","150px","50px","Black","green")}</div>
+<div onClick={()=>{alert("You hit a Button")}}>{Button("Press-me","150px","50px","Black","green")}</div>
+<div onClick={()=>{alert("A got your notification")}}>{Button("Hire me","150px","50px","Black","green")}</div>
     </div>
   );
 }
